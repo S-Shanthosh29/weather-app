1fe39f4ea87b94985879890a688434061cc6fbd9
Shanthosh <shanthoshsatchi@gmail.com>
2025-10-04 22:07:44 -0500

    Add full React app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Future Weather Forecasts</h1>
+      <p>This is showing up correctly.</p>
+    </div>
+  );
+}
+
+export default App;
+
